1274f51e64e958b6958a6d85b6f240c1c1ac9a64
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-08-10 08:58:10 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,27 +8,41 @@ class BaseModel:
   created_at = (datetime.today())
   updated_at = (datetime.today())
 
-  def __init__(self, id, name, created_at, updated_at):
-    """Initializing BaseModel object
-    args:
-      id: The uuid of the instance
-      name: the name of the object
-      created_at: The time it was created
-      updated_at: The time it was updated
-    """
-    self.id = id
-    self.name = name
-    self.created_at = created_at
-    self.updated_at = updated_at
+  def __init__(self, *args, **kwargs):
+      """Initializing BaseModel object
+      """
+      from models import storage
+      
+      if not kwargs:
+          self.id = str(uuid.uuid4())
+          self.created_at = self.updated_at = datetime.now()
+          storage.new(self)
+      else:
+          for key, value in kwargs.items():
+              if key != '__clas__':
+                  if key in ('created_at', 'updated_at'):
+                      setattr(self, key, datetime.fromisoformat(value))
+                  else:
+                      setattr(self,key, value)
 
   def __str__(self):
-    """Returns a string representation"""
-    return ([BaseModel], (self.id), self.__dict__)
+      """Returns a string representation in the format [BaseModel], (self.id), self.__dict__"""
+    
+      return [{}] ({}) {}.format(type(self).__name__, self.id, self.__dict__
+    
   def save(self):
-    """Updates the updated_at attribute with the current time"""
-    self.updated_at = datetime.now()
-    return self.updated_at
-
+      """Updates the updated_at attribute with the current time"""
+      from models import storage
+      self.updated_at = datetime.now()
+      storage.save()
+    
   def to_dict(self):
-    """returns dictionary representation of class attributes"""
-    return {'id': self.id, 'name': 'My_first_model', 'created_at': self.created_at.isoformat(), 'updated_at': self.updated_at.isoformat()}
+      """returns dictionary representation of class attributes"""
+      dict_1 = self.__dict__.copy()
+      dict_1["__class__"] = self.__class__.__name__
+      for k, v in self.__dict__.items():
+          if k in ("created_at", "updated_at"):
+              v = self.__dict__[k].isoformat()
+              dict_1[k] = v
+      return dict_1
+     
