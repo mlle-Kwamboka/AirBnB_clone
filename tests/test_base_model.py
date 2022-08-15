f7c4cfd2654de2c655431fbea59e22540d9137de
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-08-15 23:05:06 +0300

    Update test_base_model.py

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
 import unittest
-import uuid
-from models import base_models
+from uuid import uuid4
+import models
+from models.base_model import BaseModel
+from datetime import datetime
+import os
 
 class Test_Base_Models(unittest.TestCase):
-  """Unit test for testing instantiation of the BaseModel"""
+    """Unit test for testing instantiation of the BaseModel"""
 
-  def test_id(self):
-    """Testing for the id generation"""
-    B1 = BaseModel()
-    self.assertEqual(B1.id, uuid.uuid4())
-
-  def unique_id(self):
-    """Tests for unique id"""
-    B1 = Basemodel()
-    B2 = BaseModel()
-    self.assert(B1.id is not B2.id)
+    def test_id(self):
+        """Testing for the presence instance id """
+        B = BaseModel()
+        self.assertTrue(hasattr(B, "id"))
     
-  def type_id(self):
-    self.assertRaises(TypeError, type(B1.id), )
+    def str_rep(self):
+        """Tests that str representation is appropriate"""
+        self.assertEqual(str(b), "[BaseModel] ({}) {}".format(b.id, b.__dict__))
 
-  def update_time(self):
-    self.assertEqual()
+    def unique_id(self):
+        """Tests for unique id"""
+        B1 = Basemodel()
+        B2 = BaseModel()
+        self.assertNotEqual(B1.id, B2.id)
+    
+    def type_id(self):
+        """Tests that id is str"""
+        B = BaseModel()
+        self.assertTrue(Type(B.id) is str)
+        
+    def created_at_is_datetime(self):
+        """Tests that created_at is datetime"""
+        B = BaseModel()
+        self.assertTrue(type(B.created_at) is datetime)
+
+    def updated_at_is_datetime(self):
+        """Tests that updated_at is datetime"""
+        B = BaseModel()
+        self.assertTrue(type(B.updated_at) is datetime)
