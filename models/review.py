28a6dde6069cb93a7b3f0959ea7776e601f6af92
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-11-07 14:38:16 +0400

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """Defining a class Review"""
+from models.base_model import BaseModel
 
-
-class Review:
-    """Review is empty"""
-    pass
+class Review(BaseModel):
+    """a class for reviews"""
+    place_id = ""
+    user_id = ""
+    text = ""
