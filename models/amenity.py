f63dd6068b6f5723fc779088f87bf6a564e96931
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-11-07 14:31:28 +0400

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """Defining a class Amenity"""
+from models.base_model import BaseModel
 
-
-class Amenity:
-    """Amenity is empty"""
-    pass
+class Amenity(BaseModel):
+    """Amenity class"""
+    name = ""
