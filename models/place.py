08ef49312478820d6f02c952629345898857fbd2
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-11-07 14:33:49 +0400

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
 """Defining a class Place"""
-
+from models.base_model import BaseModel
 
 class Place:
-    """Place is empty"""
-    pass
+    """Class representing place"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
